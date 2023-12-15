acdf0e4467d2ffc280140c5dfa10d100d7f76f0d
john okeoghene <jonnyscience08@gmail.com>
2023-12-15 08:34:37 +0100

    Updated Configure React app for deployment to github pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import About from "./components/About";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 import Works from "./components/MyWorks";
 import Footer from "./components/Contacts";
 
 function App() {
   return (
-  <div className="bg-about-dark">
-    <Hero/>
-    <About/>
-    <Works/>
-    <Footer/>
-  </div>);
+    <div className="bg-about-dark">
+      <Hero />
+      <About />
+      <Works />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,53 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./Navigation";
 import heroBg from "../images/hero-bg-image.jpg";
 import CV from "../images/portfolio_design.jpg";
 
 const Hero = () => {
-  const headingRef = useRef();
-  const [subHdeader, setSubHeader] = useState("I'm a frontend web-developer");
-  const blinker = () => {
-    setTimeout(() => {
-      subHdeader === "I'm a frontend web-developer"
-        ? setSubHeader("I'm a frontend web-developer|")
-        : setSubHeader("I'm a frontend web-developer");
-    }, 1000);
-    return subHdeader;
-  };
+  const [output, setOutput] = useState({
+    text: "",
+    length: 0,
+    borderR: true,
+    speed: 100,
+    textNumber: true,
+  });
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setOutput((prevOutput) => {
+        const subHdeader = prevOutput.textNumber
+          ? "I'm a frontend web-developer"
+          : "I design and build user interfaces.";
+        return {
+          text:
+            prevOutput.length >= subHdeader.length + 10
+              ? prevOutput.text.slice(0, -1)
+              : prevOutput.length === subHdeader.length
+              ? prevOutput.text
+              : prevOutput.text + subHdeader.charAt(prevOutput.length),
+          length:
+            prevOutput.length === 2 * subHdeader.length + 10
+              ? 0
+              : prevOutput.length + 1,
+          borderR:
+            prevOutput.length >= subHdeader.length &&
+            prevOutput.length < subHdeader.length + 10
+              ? !prevOutput.borderR
+              : true,
+          speed:
+            prevOutput.length >= subHdeader.length &&
+            prevOutput.length < subHdeader.length + 10
+              ? 500
+              : 100,
+          textNumber:
+            prevOutput.length === 2 * subHdeader.length + 10
+              ? !prevOutput.textNumber
+              : prevOutput.textNumber,
+        };
+      });
+    }, output.speed);
+    return () => clearTimeout(timeOut);
+  });
 
   return (
     <header
@@ -27,10 +61,12 @@ const Hero = () => {
           Hi, I'm Okeoghene..
         </h1>
         <h2
-          ref={headingRef}
-          className="text-xl md:text-2xl lg:text-4xl p-1 text-center"
+          // ref={headingRef}
+          className={`text-xl md:text-2xl lg:text-4xl p-1 text-center min-h-[1.8rem] md:min-h-[2.2rem] lg:min-h-[2.8rem] ${
+            output.borderR ? "lg:border-r-4 border-r-2" : "border-none"
+          } border-gray-200`}
         >
-          {blinker()}
+          {output.text}
         </h2>
         <a
           href={CV}
